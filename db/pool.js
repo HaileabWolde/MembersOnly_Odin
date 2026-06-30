fd577317149e0d8b3b00eaffe596bd09d9dcdc00
Haileab <womahaileab1428@gmail.com>
2026-06-30 12:14:09 +0300

    started deploying mybackend on neon

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,7 @@
+const { Pool } = require("pg");
 require("dotenv").config();
-const pg = require('pg');
 
-module.exports = new pg.Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database:  process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT// The default port
-    // Insert pool options here
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
-
-
-   
